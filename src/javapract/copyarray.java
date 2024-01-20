8beea727c418ab3868fb951774871034a28fc448
rohit <rohitbramhankar@gmail.com>
2024-01-20 16:40:27 +0530

    copy the array using the arraycopy method

--- src/javapract/copyarray.java
+++ src/javapract/copyarray.java
@@ -2,13 +2,14 @@ package javapract;
 
 public class copyarray {
     public static void main(String[] args) {
+       int arr[]={1,2,3,4,5,6,7,8,9,10,22,15,46,32,81};
+      int copyto[]=new int[10];
+      System.arraycopy(arr,1,copyto,0,10);//method to copy the array
+       // System.arraycopy(arrname,start_from,copyto,startindex ,endindex);
+      for(int i=0;i<10;i++)//print the array
+      {
+          System.out.println(copyto[i]);
+      }
 
-        char arr1[] = {'a', 'b', 'c', 'd', 'f', 'h', 'j', 'k', 'l', 'm', 'n'};//copyfrom
-        char[] arr2 = new char[7];
-
-        System.arraycopy(arr1, 1,arr2,0,7);
-        System.out.println(String.valueOf(arr2));//converts different types of values into string
-
-        System.out.println(arr2);
     }
 }
